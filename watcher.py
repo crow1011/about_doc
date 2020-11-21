148bffb579862831be2ed78be61c504f6ca68c82
crow1011 <crowsmith1011>
2020-11-21 11:25:55 +0300

    create .gitignore, requirements and watcher.main draft

---
+++ watcher.py
@@ -0,0 +1,9 @@
+import requests
+
+# def main():
+#     users = get_users()
+#     todos = get_todos()
+#     status = check_exists()
+#     if status:
+#         res = create_report(users, todos)
+#      return res
